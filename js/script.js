4148f31d1c066b502692a68cc3943097d0865005
uchenna <okorocode@gmail.com>
2019-02-09 00:36:22 +0100

    fetching projects from github

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 console.log('working');
 
+
+fetch('https://api.github.com/users/UcheSylvester/repos')
+.then(response => response.json())
+.then(projects => {
+    for (project of projects) {
+        if(!project.fork) {
+            console.log(project)
+        }
+        // console.log(project.fork)
+    }
+})
