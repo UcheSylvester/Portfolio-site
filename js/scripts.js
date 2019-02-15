2602520f8b58b68a7164db2b316ba34587d29a60
uchenna <okorocode@gmail.com>
2019-02-15 06:08:00 +0100

    fetched projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log('working....')
+console.log('working....');
+
+function fetchProjects() {
+    const projectsUrl = 'https://api.github.com/users/uchesylvester';
+
+    fetch(projectsUrl)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+}
+
+document.addEventListener('DOMcontentLoaded', function(e) {
+    console.log(e)
+})
