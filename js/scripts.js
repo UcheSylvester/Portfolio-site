49a7300b67977248ac3ba22538f2504147087dee
uchenna <okorocode@gmail.com>
2019-02-16 21:28:53 +0100

    working on fetching projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
 console.log('working....');
 
 function fetchProjects() {
-    const projectsUrl = 'https://api.github.com/users/uchesylvester';
+    const projectsUrl = 'https://api.github.com/users/uchesylvester/repos';
+
+    let htmlContent = '';
+    const portfolio = document.querySelector('#portfolio');
 
     fetch(projectsUrl)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+    .then(function(response) {
+        return response.json()
     })
+    .then(function(projects) {
+
+        for (const project of projects) {
+            if(!project.fork) {
+                console.log(project.name);
+                htmlContent =  `
+                ${project.name}
+                `
+            }
+        }
+    })
+
+    portfolio.insertAdjacentHTML('afterbegin', htmlContent)
+    
 }
 
-document.addEventListener('DOMcontentLoaded', function(e) {
-    console.log(e)
-})
+
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    fetchProjects()
+});
+
+// fetchProjects()
